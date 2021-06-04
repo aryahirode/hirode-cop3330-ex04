f9885ab6aca5e6c7b64b167b9283092b6e80ade0
Arya Hirode <aryahirode2001@gmail.com>
2021-06-04 15:32:01 -0400

    Wrote nounPrompt()

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -4,8 +4,23 @@
  */
 package base;
 
+import java.util.Scanner;
+
+/*
+Story:
+    There was a (adjective)(noun) that liked to (adverb)(verb).
+    Then it lived happily ever after.
+ */
 public class App {
+    static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
-        
+
     }
+
+    private String nounPrompt() {
+        System.out.print("Enter a noun: ");
+        return in.nextLine();
+    }
+
+
 }
